a80309b3220b53464214198f21419f8168709119
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-09-14 14:51:33 +0900

    uqunie: fanzi > config.php

---
+++ fanzi/config.php
@@ -0,0 +1,20 @@
+<?php
+/**	op-public-uqunie-org:/fanzi/config.php
+ *
+ * @created    2026-09-14
+ * @license    Apache-2.0
+ * @package    op-public-uqunie-org
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+//	...
+OP()->Config('app',[
+	'title'       => 'Fanziquan Practice Group | Chinese Martial Arts for Beginners',
+	'copyright'   => 'Copyright 2026 翻子拳 All Rights Reserved.',
+	'description' => 'Fanziquan Practice Group | Chinese Martial Arts for Beginners. Beginners are welcome to practice with us; observation is also available.',
+]);
